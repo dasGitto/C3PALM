1ecc2cc8943fa2d554ca9d85b98fe1504729da24
DasGitto <mafyvic@gmail.com>
2025-01-19 11:31:14 +0100

    initial commit

---
+++ effects.ino
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+// Abstract function declarations for each effect
+void fireEffect();
+
+void meteorEffect();
+
+void strobeEffect();
+
+void glowEffect();
+
+void randomEffect();
